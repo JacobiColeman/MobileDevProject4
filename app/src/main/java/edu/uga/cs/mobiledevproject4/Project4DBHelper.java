388072b70ffe3e3cfd15cdcbbe26b9beb60bca18
JacobiColeman <112585897+JacobiColeman@users.noreply.github.com>
2022-11-01 10:28:23 -0400

    +Added table in DB helper

--- app/src/main/java/edu/uga/cs/mobiledevproject4/Project4DBHelper.java
+++ app/src/main/java/edu/uga/cs/mobiledevproject4/Project4DBHelper.java
@@ -22,19 +22,44 @@ public class Project4DBHelper extends SQLiteOpenHelper {
     public static final String PROJECT4_COLUMN_CAPITAL_SINCE = "capital since";
     public static final String PROJECT4_COLUMN_CAPITAL_RANK = "capital rank";
 
+    public static final String TABLE_STATEQUIZ = "state quiz";
+    public static final String STATE_QUIZ_COLUMN_ID = "_id";
+    public static final String STATE_QUIZ_COLUMN_DATE = "date";
+    public static final String STATE_QUIZ_COLUMN_Q1 = "question one";
+    public static final String STATE_QUIZ_COLUMN_Q2 = "question two";
+    public static final String STATE_QUIZ_COLUMN_Q3 = "question three";
+    public static final String STATE_QUIZ_COLUMN_Q4 = "question four";
+    public static final String STATE_QUIZ_COLUMN_Q5 = "question five";
+    public static final String STATE_QUIZ_COLUMN_Q6 = "question six";
+    public static final String STATE_QUIZ_COLUMN_NUMCORRECT = "number correct";
+    public static final String STATE_QUIZ_COLUMN_QUESTIONS_ANSWERED = "questions answered";
 
     private static Project4DBHelper helperInstance;
 
 
     private static final String CREATE_PROJECT4 =
-            "create table " + TABLE_PROJECT4 + " ("
+            "create table " + TABLE_PROJECT4 + "("
                     + PROJECT4_COLUMN_STATE + " TEXT PRIMARY KEY, "
-                    + PROJECT4_COLUMN_CAPITAL_CITY + " TEXT , "
+                    + PROJECT4_COLUMN_CAPITAL_CITY + " TEXT, "
                     + PROJECT4_COLUMN_SECOND_CITY + " TEXT, "
                     + PROJECT4_COLUMN_THIRD_CITY + " TEXT, "
                     + PROJECT4_COLUMN_STATEHOOD + " TEXT, "
                     + PROJECT4_COLUMN_CAPITAL_SINCE + " TEXT,"
                     + PROJECT4_COLUMN_CAPITAL_RANK + " INTEGER, "
+                    + ")";
+
+    private static final String CREATE_STATEQUIZ =
+            "create table " + TABLE_STATEQUIZ + "( "
+                    + STATE_QUIZ_COLUMN_ID  + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + STATE_QUIZ_COLUMN_DATE + " TEXT, "
+                    + STATE_QUIZ_COLUMN_Q1 + " TEXT, "
+                    + STATE_QUIZ_COLUMN_Q2 + " TEXT, "
+                    + STATE_QUIZ_COLUMN_Q3 + " TEXT, "
+                    + STATE_QUIZ_COLUMN_Q4 + " TEXT, "
+                    + STATE_QUIZ_COLUMN_Q5 + " TEXT, "
+                    + STATE_QUIZ_COLUMN_Q6 + " TEXT, "
+                    + STATE_QUIZ_COLUMN_NUMCORRECT + " INTEGER, "
+                    + STATE_QUIZ_COLUMN_QUESTIONS_ANSWERED + " INTEGER, "
                     + ")";
 
 
@@ -56,6 +81,9 @@ public class Project4DBHelper extends SQLiteOpenHelper {
     public void onCreate( SQLiteDatabase db ) {
         db.execSQL( CREATE_PROJECT4 );
         Log.d( DEBUG_TAG, "Table " + TABLE_PROJECT4 + " created" );
+
+        db.execSQL( CREATE_STATEQUIZ );
+        Log.d( DEBUG_TAG, "Table " + TABLE_STATEQUIZ + " created" );
     }
 
 
